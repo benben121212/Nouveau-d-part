6116444183a52e9ef8a27a400903f8bf4e9e8479
benben121212 <benoitdelpontnew@gmail.com>
2023-04-23 13:14:31 +0200

    dur

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const sectionNames = ['Accueil', 'Monde', 'Histoire', 'Personnage principal', 'Personnages secondaires', 'Faunes', 'Véhicules'];
-const sectionIDs = ['accueil', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7'];
+const sectionNames = ['Menu', 'Accueil', 'Monde', 'Histoire', 'Personnage principal', 'Personnages secondaires', 'Faunes', 'Véhicules'];
+const sectionIDs = ['sectionmenu', 'sectionaccueil', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7'];
 
 const sections = document.querySelectorAll('.section');
 const sectionHeights = [...sections].map(section => section.offsetHeight);
@@ -14,23 +14,44 @@ const section6Start = section5End;
 const section6End = section6Start + sectionHeights[5];
 const section7Start = section6End;
 
+function toggleMenu() {
+  var nav = document.querySelector('.menu-deroulant nav');
+  nav.classList.toggle('menu-deroulant--visible');
+}
+
 const setActiveSection = (scrollPosition) => {
+  let delay = 0;
   sections.forEach((section, index) => {
-    section.classList.remove('is-active');
+    section.classList.remove('is-active', 'is-fixed');
     if (scrollPosition >= section3Start && scrollPosition < section4Start && index === 2) {
-      section.classList.add('is-active');
+      setTimeout(() => {
+        section.classList.add('is-active');
+      }, delay);
+      delay += 200;
     }
     if (scrollPosition >= section4Start && scrollPosition < section5Start && index === 3) {
-      section.classList.add('is-active');
+      setTimeout(() => {
+        section.classList.add('is-active', 'is-fixed');
+      }, delay);
+      delay += 200;
     }
     if (scrollPosition >= section5Start && scrollPosition < section6Start && index === 4) {
-      section.classList.add('is-active');
+      setTimeout(() => {
+        section.classList.add('is-active', 'is-fixed');
+      }, delay);
+      delay += 200;
     }
     if (scrollPosition >= section6Start && scrollPosition < section7Start && index === 5) {
-      section.classList.add('is-active');
+      setTimeout(() => {
+        section.classList.add('is-active', 'is-fixed');
+      }, delay);
+      delay += 200;
     }
     if (scrollPosition >= section7Start && index === 6) {
-      section.classList.add('is-active');
+      setTimeout(() => {
+        section.classList.add('is-active', 'is-fixed');
+      }, delay);
+      delay += 200;
     }
   });
 };
