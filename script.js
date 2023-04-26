94111e8d2a1b1fa51ed196cfb0568415a1bd5633
benben121212 <benoitdelpontnew@gmail.com>
2023-04-26 21:03:45 +0200

    bouton recherche

--- script.js
+++ script.js
@@ -26,28 +26,3 @@ detectActiveSection();
 
 window.addEventListener("scroll", detectActiveSection);
 
-// Bouton selecteur jeu video
-document.getElementById("btnSections").onclick = function() {
-    var sectionsContainer = document.getElementById("sectionsContainer");
-    sectionsContainer.classList.toggle("sections-visible");
-    if (sectionsContainer.classList.contains("sections-visible")) {
-      document.getElementById("btnSections").innerHTML = "Masquer les sections";
-    } else {
-      document.getElementById("btnSections").innerHTML = "Afficher les sections";
-    }
-  }
-  // Bouton selecteur jeu video
-function toggleSections() {
-    var sectionsContainer = document.getElementById("sectionsContainer");
-    var btnSections = document.getElementById("btnSections");
-    if (sectionsContainer.classList.contains("sections-visible")) {
-      sectionsContainer.classList.remove("sections-visible");
-      btnSections.innerHTML = "Afficher les sections";
-    } else {
-      sectionsContainer.classList.add("sections-visible");
-      btnSections.innerHTML = "Masquer les sections";
-    }
-  }
-  
-  document.getElementById("btnSections").onclick = toggleSections;
-  
